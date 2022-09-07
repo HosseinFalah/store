86136403c1db285af7325215187c4e38c3fb6029
HosseinFalah <hosseinfalah2021@gmail.com>
2022-09-07 09:05:56 +0430

    fix api productsDetails

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles"
+import { Button } from "@mui/material";
+
+const theme = createTheme({
+    typography: {
+        fontFamily: "IRANSansDN",
+    },
+    direction: "rtl"
+})
+
 const App = () => {
     return (
-        <div>App</div>
+        <ThemeProvider theme={theme}>
+            <div>
+                <Button variant="contained" color="success">اضافه کردن</Button>
+            </div>
+        </ThemeProvider>
     )
 }
 
